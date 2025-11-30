e5a1758bc846a1838b52d3a7ea94840b873dbdd6
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2025-11-30 17:33:20 +0530

    feat: set up server and add deployment configuration; initialize package and lock files

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoute from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
-
+import path from "path"
 dotenv.config();
 
 const app = express()
+const __dirname = path.resolve();
+
 
 app.get('/',(req,res)=>{
     res.send("App server is running")
@@ -13,6 +15,16 @@ app.get('/',(req,res)=>{
 
 app.use('/api/auth', authRoute)
 app.use("/api/mess",messageRoute)
+
+// make ready for deployment
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/chatApp/dist")))
+
+    app.get("*", (_,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/chatApp/","dist","index.html"))
+    })
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
